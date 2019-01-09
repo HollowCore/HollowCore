111a8ceae2e51bb100e30a8a3fa935f8966660fb
Braden.Scothern <BradenScothern@gmail.com>
2019-01-08 22:21:40 -0700

    fixing build issue on linux

--- HCCore.h
+++ HCCore.h
@@ -13,6 +13,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 //----------------------------------------------------------------------------------------------------------------------------------
 // MARK: - Object Foundation
