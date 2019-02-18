416e942b0f812bd529c967f73f783dc1c3884942
Matt <matt.stoker@gmail.com>
2019-02-18 00:58:43 -0700

    Added object type queries

---
+++ Source/JSON/HCJSON.c
@@ -0,0 +1,9 @@
+//
+//  HCJSON.c
+//  HollowCore
+//
+//  Created by Matt Stoker on 2/17/19.
+//  Copyright © 2019 HollowCore. All rights reserved.
+//
+
+#include "HCJSON.h"
---
+++ Source/JSON/HCJSON.h
@@ -0,0 +1,14 @@
+//
+//  HCJSON.h
+//  HollowCore
+//
+//  Created by Matt Stoker on 2/17/19.
+//  Copyright © 2019 HollowCore. All rights reserved.
+//
+
+#ifndef HCJSON_h
+#define HCJSON_h
+
+#include <stdio.h>
+
+#endif /* HCJSON_h */
