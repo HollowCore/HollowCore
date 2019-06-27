3b323d5ccc9ee85593bb3eedee1e8ce210a49d80
Braden Scothern <BradenScothern@gmail.com>
2019-06-26 22:37:42 -0600

    More attempts to install Valgrind on CI

--- Tools/travis_before_install.sh
+++ Tools/travis_before_install.sh
@@ -5,6 +5,8 @@
 ###################################################################
 function before_linux () {
     if [[ ${NAME} == "ValgrindTest" ]]; then
+        sudo apt-get update
+        sudo apt-get install libc6-dbg
         sudo apt-get install -f valgrind
     fi
 }
