65942014269cae8cbc91f39f83aa3d48b4d0f3d6
Matt <matt.stoker@gmail.com>
2020-02-23 16:58:55 -0700

    More Package.swift

--- Package.swift
+++ Package.swift
@@ -9,16 +9,47 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "HollowCore",
-            targets: ["HollowCore"]),
+            targets: [
+                "HollowCoreCore",
+                "HollowCoreData",
+                "HollowCoreContainer",
+                "HollowCoreGeometry",
+                "HollowCoreGraphic",
+                "HollowCoreJSON",
+                "HollowCoreThread",
+        ]),
     ],
     dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "HollowCore",
-            dependencies: [],
-            path: "./Source",
+            name: "HollowCoreCore",
+            path: "./Source/Core",
+            publicHeadersPath: "."),
+        .target(
+            name: "HollowCoreData",
+            path: "./Source/Data",
+            publicHeadersPath: "."),
+        .target(
+            name: "HollowCoreContainer",
+            path: "./Source/Container",
+            publicHeadersPath: "."),
+        .target(
+            name: "HollowCoreGeometry",
+            path: "./Source/Geometry",
+            publicHeadersPath: "."),
+        .target(
+            name: "HollowCoreGraphic",
+            path: "./Source/Graphic",
+            publicHeadersPath: "."),
+        .target(
+            name: "HollowCoreJSON",
+            path: "./Source/JSON",
+            publicHeadersPath: "."),
+        .target(
+            name: "HollowCoreThread",
+            path: "./Source/Thread",
             publicHeadersPath: "."),
     ]
 )
