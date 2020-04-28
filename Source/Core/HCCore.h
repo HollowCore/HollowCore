befdff790ad0bea4c6950bc232b44b03335a94b5
Matt Stoker <matt.stoker@gmail.com>
2020-04-27 23:41:31 -0600

    Documented HCCore

--- Source/Core/HCCore.h
+++ Source/Core/HCCore.h
@@ -10,7 +10,6 @@
 #ifndef HCCore_h
 #define HCCore_h
 
-#include <stdatomic.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,61 +20,160 @@
 //----------------------------------------------------------------------------------------------------------------------------------
 // MARK: - Value Types
 //----------------------------------------------------------------------------------------------------------------------------------
+
+/// A value containing @c false or @c true.
+///
+/// Has a bit width of at least 1.
+/// A value of @c false is represented by all bits being @c 0.
+/// All other values are considered to be @c true.
 typedef bool HCBoolean;
+
+/// A signed integer value.
+///
+/// Has a bit width of exactly 64.
+/// Negative values are represented using 2s complement representation.
 typedef int64_t HCInteger;
+
+/// A signed floating-point value.
+///
+/// Has a bit width of exactly 64.
+/// Uses IEEE 754 representation.
 typedef double HCReal;
 
 //----------------------------------------------------------------------------------------------------------------------------------
 // MARK: - Extents
 //----------------------------------------------------------------------------------------------------------------------------------
-#define HCIntegerMin (INT64_MIN)
-#define HCIntegerMax (INT64_MAX)
+/// The minimum value an integer may contain. Is guaranteed negative.
+#define HCIntegerMinimum (INT64_MIN)
 
-#define HCRealMin (-DBL_MAX)
-#define HCRealMax (DBL_MAX)
+/// The maximum value an integer may contain. Is guaranteed positive.
+#define HCIntegerMaximum (INT64_MAX)
 
-//----------------------------------------------------------------------------------------------------------------------------------
-// MARK: - Atomic Value Types
-//----------------------------------------------------------------------------------------------------------------------------------
-typedef _Atomic HCBoolean HCAtomicBoolean;
-typedef _Atomic HCInteger HCAtomicInteger;
+/// The minimum finite negative value a real may contain. Adjacent to negative infinity.
+#define HCRealMinimumNegative (-DBL_MAX)
+
+/// The maximum finite negative value a real may contain. Adjacent to zero.
+#define HCRealMaximumNegative (-DBL_TRUE_MIN)
+
+/// The maximum finite positive value a real may contain. Adjacent to zero.
+#define HCRealMinimumPositive (DBL_TRUE_MIN)
+
+/// The maximum finite positive value a real may contain. Adjacent to positive infinity.
+#define HCRealMaximumPositive (DBL_MAX)
 
 //----------------------------------------------------------------------------------------------------------------------------------
 // MARK: - Memory Types
 //----------------------------------------------------------------------------------------------------------------------------------
+
+/// A unit of storage of an 8-bit width.
 typedef uint8_t HCByte;
 
 //----------------------------------------------------------------------------------------------------------------------------------
 // MARK: - Boolean Operations
 //----------------------------------------------------------------------------------------------------------------------------------
+
+/// Determines equality of a pair of boolean values.
+///
+/// If @c HCBooleanIsEqual() returns @c true for a pair of boolean values, @c HCBooleanHashValue() will return the same value for them.
+///
+/// @param self A boolean value to equate.
+/// @param other Another boolean value to equate.
+/// @returns @c true if @c self and @c other both contain @c false or both do not contain @c false. Returns @c false otherwise.
 HCBoolean HCBooleanIsEqual(HCBoolean self, HCBoolean other);
+
+/// Provides a hashing value corresponding to a boolean value.
+///
+/// @param self Boolean value to hash.
+/// @returns A value suitable to use as a hash value to represent @c self or to combine with other values to form an aggregate hash value.
 HCInteger HCBooleanHashValue(HCBoolean self);
+
+/// Prints a text representation of a boolean value to a stream.
+/// @param self A boolean value to print.
+/// @param stream Stream to which @c self will be printed.
 void HCBooleanPrint(HCBoolean self, FILE* stream);
 
 //----------------------------------------------------------------------------------------------------------------------------------
 // MARK: - Integer Operations
 //----------------------------------------------------------------------------------------------------------------------------------
+
+/// Determines equality of a pair of integer values.
+///
+/// If @c HCIntegerIsEqual() returns @c true for a pair of integer values, @c HCIntegerHashValue() will return the same value for them.
+///
+/// @param self An integer value to equate.
+/// @param other Another integer value to equate.
+/// @returns @c true if @c self and @c other both contain the same integer value. Returns @c false otherwise.
 HCBoolean HCIntegerIsEqual(HCInteger self, HCInteger other);
+
+/// Provides a hashing value corresponding to an integer value.
+///
+/// @param self An integer value to hash.
+/// @returns A value suitable to use as a hash value to represent @c self or to combine with other values to form an aggregate hash value.
 HCInteger HCIntegerHashValue(HCInteger self);
+
+/// Prints a text representation of an integer value to a stream.
+/// @param self Integer value to print.
+/// @param stream Stream to which @c self will be printed.
 void HCIntegerPrint(HCInteger self, FILE* stream);
 
 //----------------------------------------------------------------------------------------------------------------------------------
 // MARK: - Real Operations
 //----------------------------------------------------------------------------------------------------------------------------------
+
+/// Determines approximate equality of a pair of real values.
+///
+/// @param self A real value to equate.
+/// @param other Another real value to equate.
+/// @param dissimilarity The distance @c self can be from @c other before they are considered dissimilar.
+/// @returns @c true if the difference of @c self and @c other are less than dissimilarity. Returns @c false otherwise.
 HCBoolean HCRealIsSimilar(HCReal self, HCReal other, HCReal dissimilarity);
+
+/// Determines equality of a pair of real values.
+///
+/// If @c HCRealIsEqual() returns @c true for a pair of real values, @c HCRealHashValue() will return the same value for them.
+///
+/// @param self A real value to equate.
+/// @param other Another real value to equate.
+/// @returns @c true if @c self and @c other  are finite and contain the same real value. Returns @c false otherwise.
+/// @todo What about @c NAN? @c +Inf? @c -Inf?
 HCBoolean HCRealIsEqual(HCReal self, HCReal other);
+
+/// Provides a hashing value corresponding to a real value.
+///
+/// @param self A real value to hash.
+/// @returns A value suitable to use as a hash value to represent @c self or to combine with other values to form an aggregate hash value. In addition, for values of @c self exactly representing the same value as an @c HCInteger value, this will be the same value as returned from @c HCIntegerHashValue().
 HCInteger HCRealHashValue(HCReal self);
+
+/// Prints a text representation of a real value to a stream.
+/// @param self Real value to print.
+/// @param stream Stream to which @c self will be printed.
 void HCRealPrint(HCReal self, FILE* stream);
 
 //----------------------------------------------------------------------------------------------------------------------------------
 // MARK: - Object Foundation
 //----------------------------------------------------------------------------------------------------------------------------------
+
+/// The name of a type.
+///
+/// Contains the same value for all instances of a type.
 typedef const char* const HCTypeName;
+
+/// Information about a specific type.
+///
+/// Contains the same value for all instances of a type.
 typedef const struct HCTypeData* HCType;
+
+/// Information related to a type common to all instances of that type.
+///
+/// Includes identifying information for a type and the ancestors of that type.
 typedef const struct HCTypeData {
     HCTypeName name;
     HCType ancestor;
 } HCTypeData;
+
+/// A reference to an instance of an object.
+///
+/// Used when the specific reference type of an object cannot be used.
 typedef void* HCRef;
 
 #endif /* HCCore_h */
